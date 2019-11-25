1535186c160159e2ae3bb16c66aad5916f67f837
David Ortinau <david.ortinau@microsoft.com>
2019-11-24 20:49:56 -0600

    added apple wallet example, lufthansa

--- CarouselGallery/CarouselGallery.Android/MainActivity.cs
+++ CarouselGallery/CarouselGallery.Android/MainActivity.cs
@@ -22,7 +22,7 @@ namespace CarouselGallery.Droid
             SetStatusBarColor(Android.Graphics.Color.Black);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.SetFlags("CarouselView_Experimental");
+            global::Xamarin.Forms.Forms.SetFlags("CarouselView_Experimental", "IndicatorView_Experimental");
             Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
