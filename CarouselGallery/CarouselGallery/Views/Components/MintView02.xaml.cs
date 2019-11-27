dfd31152d85829010f7a12333cbbbf2389199a29
David Ortinau <david.ortinau@microsoft.com>
2019-11-26 18:47:24 -0600

    stubbed out 2 ugly mint views

---
+++ CarouselGallery/CarouselGallery/Views/Components/MintView02.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselGallery.Views.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MintView02
+    {
+        public MintView02()
+        {
+            InitializeComponent();
+        }
+    }
+}
