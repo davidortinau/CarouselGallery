f182b64383a6323d26f8cd224b96506adcdf3bc4
David Ortinau <david.ortinau@microsoft.com>
2019-11-27 14:20:50 -0600

    instagram sample that swaps description as item changes

---
+++ CarouselGallery/CarouselGallery/Models/InstaPic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselGallery.Models
+{
+    public class InstaPic
+    {
+        public string ImageSource { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CarouselGallery/CarouselGallery/Views/InstagramPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CarouselGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselGallery.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InstagramPage : ContentPage
+    {
+        public InstagramPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void cv_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
+        {
+            Description.Text = (e.CurrentItem as InstaPic).Description;
+        }
+    }
+}
